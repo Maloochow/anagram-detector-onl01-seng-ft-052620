a9538cea2ebfe3b9c9e4b8d0c330b475e4a90e62
Maloochow <luyao.mly@gmail.com>
2020-06-06 00:11:06 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        match_array = []
+        array.each do |item|
+        match_array << item if item.split("").sort == @word.split("").sort
+        end
+        match_array
+
+        #alternatively
+        # array.select do |item|
+        # item.split("").sort == @word.split("").sort
+        # end
+    end
+end
